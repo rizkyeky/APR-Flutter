e04eaa47a85a95753505621c3aaea076dcd280e3
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2021-03-10 22:43:23 +0700

    update

--- lib/view/page/pelatihan.dart
+++ lib/view/page/pelatihan.dart
@@ -16,99 +16,107 @@ class PelatihanPage extends Page<PelatihanBloc> {
         )),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          const SizedBox(height: 24,),
-          ContainerList<Pelatihan>(
-            containerCount: 3,
-            future: bloc.kategoriService.getPelatihan,
-            bottomBuilder: (context, index, data) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(bloc.listOfContainer2[index]['title'], style: textTheme.subtitle1.copyWith(
-                  height: 1.2,
-                  color: colorScheme['primary'],
-                  fontWeight: FontWeight.bold,
-                )),
-                Text(bloc.listOfContainer2[index]['subtitle'], style: textTheme.bodyText2)
-              ],
-            ),
-            insideBuilder: (context, index, data) => Padding(
-              padding: const EdgeInsets.only(right: 12, top: 12),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: SimpleChip(
-                  color: Colors.white,
-                  child: Text(bloc.listOfContainer2[index]['chip'], style: textTheme.subtitle2)
+      body: RefreshIndicator(
+        onRefresh: () async {
+          Future.delayed(const Duration(seconds: 1))
+            .whenComplete(() => injector.rebuild());
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 24,),
+              ContainerList<Pelatihan>(
+                containerCount: 3,
+                future: bloc.kategoriService.getPelatihan,
+                bottomBuilder: (context, index, data) => Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(data.nama, style: textTheme.subtitle1.copyWith(
+                      height: 1.2,
+                      color: colorScheme['primary'],
+                      fontWeight: FontWeight.bold,
+                    )),
+                    Text(bloc.listOfContainer2[index]['subtitle'], style: textTheme.bodyText2)
+                  ],
                 ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 24,),
-          StatefulBuilder(
-            builder: (context, setState) => Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                  child: ListView.separated(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: bloc.listOfKategori.length,
-                    separatorBuilder: (_, index) => const SizedBox(width: 12,),
-                    itemBuilder: (_, index) => SimpleChip(
-                      color: index == indexOfKategori1 ? colorScheme['primary'] : colorScheme['background3'],
-                      onTap: () => setState(() {
-                        if (indexOfKategori1 != index) {
-                          indexOfKategori1 = index;
-                        }
-                      }),
-                      child: Text(bloc.listOfKategori[index], style: textTheme.subtitle2.copyWith(
-                        color: index == indexOfKategori1 ? Colors.white : colorScheme['text1']
-                      )),
-                    )
+                insideBuilder: (context, index, data) => Padding(
+                  padding: const EdgeInsets.only(right: 12, top: 12),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: SimpleChip(
+                      color: Colors.white,
+                      child: Text(bloc.listOfContainer2[index]['chip'], style: textTheme.subtitle2)
+                    ),
                   ),
                 ),
-                ... List.generate(4, (index) => ContainerTile(
-                  openWidget: PelatihanDetailPage(),
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ))
-              ],
-            )
-          ),
-          const SizedBox(height: 24),
-          StatefulBuilder(
-            builder: (context, setState) => Column(
-              children: [
-                Wrap(
-                  spacing: 12,
-                  runSpacing: 12,
-                  children: List.generate(bloc.listOfKategori2.length, (index) => SizedBox(
-                    width: injector.screenWidth/2 - 36,
-                    child: OptionButton(
-                      label: bloc.listOfKategori2[index]['label'],
-                      labelStyle: textTheme.subtitle1.copyWith(
-                        color: (indexOfKategori2 == index) ? Colors.white : colorScheme['text1']
+              ),
+              const SizedBox(height: 24,),
+              StatefulBuilder(
+                builder: (context, setState) => Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: ListView.separated(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: bloc.listOfKategori.length,
+                        separatorBuilder: (_, index) => const SizedBox(width: 12,),
+                        itemBuilder: (_, index) => SimpleChip(
+                          color: index == indexOfKategori1 ? colorScheme['primary'] : colorScheme['background3'],
+                          onTap: () => setState(() {
+                            if (indexOfKategori1 != index) {
+                              indexOfKategori1 = index;
+                            }
+                          }),
+                          child: Text(bloc.listOfKategori[index], style: textTheme.subtitle2.copyWith(
+                            color: index == indexOfKategori1 ? Colors.white : colorScheme['text1']
+                          )),
+                        )
                       ),
-                      icon: SvgPicture.asset(bloc.listOfKategori2[index]['icon'],
-                        color: (indexOfKategori2 == index) ? Colors.white : colorScheme['text1'],
-                        height: 24,
-                      ),
-                      color: (indexOfKategori2 == index) ? colorScheme['primary'] : null,
-                      onTap: () => setState(() {
-                        indexOfKategori2 = index;
-                      }),
                     ),
-                  ))
-                ),
-                const SizedBox(height: 24,),
-                ... List.generate(4, (index) => ContainerTile(
-                  openWidget: PelatihanDetailPage(),
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ))
-              ],
-            )
-          )
-        ],
+                    ... List.generate(4, (index) => ContainerTile(
+                      openWidget: PelatihanDetailPage(),
+                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    ))
+                  ],
+                )
+              ),
+              const SizedBox(height: 24),
+              StatefulBuilder(
+                builder: (context, setState) => Column(
+                  children: [
+                    Wrap(
+                      spacing: 12,
+                      runSpacing: 12,
+                      children: List.generate(bloc.listOfKategori2.length, (index) => SizedBox(
+                        width: injector.screenWidth/2 - 36,
+                        child: OptionButton(
+                          label: bloc.listOfKategori2[index]['label'],
+                          labelStyle: textTheme.subtitle1.copyWith(
+                            color: (indexOfKategori2 == index) ? Colors.white : colorScheme['text1']
+                          ),
+                          icon: SvgPicture.asset(bloc.listOfKategori2[index]['icon'],
+                            color: (indexOfKategori2 == index) ? Colors.white : colorScheme['text1'],
+                            height: 24,
+                          ),
+                          color: (indexOfKategori2 == index) ? colorScheme['primary'] : null,
+                          onTap: () => setState(() {
+                            indexOfKategori2 = index;
+                          }),
+                        ),
+                      ))
+                    ),
+                    const SizedBox(height: 24,),
+                    ... List.generate(4, (index) => ContainerTile(
+                      openWidget: PelatihanDetailPage(),
+                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    ))
+                  ],
+                )
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
